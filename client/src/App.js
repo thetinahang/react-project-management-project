f65d1e47238649cd32616344e9950db897afad49
thetinahang <thetinahang@gmail.com>
2019-03-12 21:07:05 -0700

    add container and navbar, navbar not working correctly

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import ProjectsContainer from './containers/ProjectsContainer';
+
 
 class App extends Component {
 
@@ -15,25 +17,34 @@ class App extends Component {
       .catch(error => console.log(error));
   }
 
-  render() {
+render() {
     return (
       <div className="App">
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand href="#home">NREL Buildings Database</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="https://developer.nrel.gov/docs/buildings/building-case-studies/" target="_blank">NREL Developer Link</Nav.Link>
+              <NavDropdown title="Actions" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Tina's React+Redux Project</h1>
+          <h3>Making use of in-depth information and data on high-performance, green building projects across the United States and abroad. </h3>
         </header>
+        <ProjectsContainer projects={this.state.projects}/>
+        
       </div>
-    );
+    )
   }
 }
 
---
+++ client/src/containers/ProjectsContainer.js
@@ -0,0 +1,18 @@
+// container component
+import React, { Component } from 'react';
+
+class ProjectsContainer extends Component {
+
+	render() {
+		return (
+			<div className="projects-container">
+				<h1>Hello from ProjectsContainer</h1>
+			</div>
+		);
+	}
+};
+
+export default ProjectsContainer;
+
+
+
